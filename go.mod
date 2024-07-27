6a7e239276b8d28db0b409a4a853bfba16148ff9
sewn <sewn@disroot.org>
2024-07-27 23:05:42 +0300

    print message type name, handle more message types, support embeds

--- go.mod
+++ go.mod
@@ -2,18 +2,10 @@ module github.com/apprehensions/kiroku
 
 go 1.20
 
-require (
-	github.com/diamondburned/arikawa/v3 v3.3.3
-	github.com/fatih/color v1.15.0
-)
+require github.com/diamondburned/arikawa/v3 v3.3.6
 
 require (
-	github.com/gorilla/schema v1.2.1 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	github.com/gorilla/schema v1.4.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
--- kiroku.go
+++ kiroku.go
@@ -6,17 +6,51 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/state"
 )
 
-func main() {
-	const messagesFetchLimit = 1024
+var typeNames = map[discord.MessageType]string{
+	discord.DefaultMessage:                             "DefaultMessage",
+	discord.RecipientAddMessage:                        "RecipientAddMessage",
+	discord.RecipientRemoveMessage:                     "RecipientRemoveMessage",
+	discord.CallMessage:                                "CallMessage",
+	discord.ChannelNameChangeMessage:                   "ChannelNameChangeMessage",
+	discord.ChannelIconChangeMessage:                   "ChannelIconChangeMessage",
+	discord.ChannelPinnedMessage:                       "ChannelPinnedMessage",
+	discord.GuildMemberJoinMessage:                     "GuildMemberJoinMessage",
+	discord.NitroBoostMessage:                          "NitroBoostMessage",
+	discord.NitroTier1Message:                          "NitroTier1Message",
+	discord.NitroTier2Message:                          "NitroTier2Message",
+	discord.NitroTier3Message:                          "NitroTier3Message",
+	discord.ChannelFollowAddMessage:                    "ChannelFollowAddMessage",
+	discord.GuildDiscoveryDisqualifiedMessage:          "GuildDiscoveryDisqualifiedMessage",
+	discord.GuildDiscoveryRequalifiedMessage:           "GuildDiscoveryRequalifiedMessage",
+	discord.GuildDiscoveryGracePeriodInitialWarning:    "GuildDiscoveryGracePeriodInitialWarning",
+	discord.GuildDiscoveryGracePeriodFinalWarning:      "GuildDiscoveryGracePeriodFinalWarning",
+	discord.ThreadCreatedMessage:                       "ThreadCreatedMessage",
+	discord.InlinedReplyMessage:                        "InlinedReplyMessage",
+	discord.ChatInputCommandMessage:                    "ChatInputCommandMessage",
+	discord.ThreadStarterMessage:                       "ThreadStarterMessage",
+	discord.GuildInviteReminderMessage:                 "GuildInviteReminderMessage",
+	discord.ContextMenuCommand:                         "ContextMenuCommand",
+	discord.AutoModerationActionMessage:                "AutoModerationActionMessage",
+	discord.RoleSubscriptionPurchaseMessage:            "RoleSubscriptionPurchaseMessage",
+	discord.InteractionPremiumUpsellMessage:            "InteractionPremiumUpsellMessage",
+	discord.StageStartMessage:                          "StageStartMessage",
+	discord.StageEndMessage:                            "StageEndMessage",
+	discord.StageSpeakerMessage:                        "StageSpeakerMessage",
+	discord.StageTopicMessage:                          "StageTopicMessage",
+	discord.GuildApplicationPremiumSubscriptionMessage: "GuildApplicationPremiumSubscriptionMessage",
+}
 
+func main() {
 	token := flag.String("token", "", "Discord Token")
 	cid := flag.Int64("channel", 0, "Channel ID")
+	limit := flag.Uint("limit", 0, "messages to fetch (0 for unlimited)")
 	flag.Parse()
 
 	s := state.New(*token)
@@ -35,62 +69,105 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var lastMessageID discord.MessageID
+	ms, err := s.MessagesBefore(c.ID, discord.MessageID(0), *limit)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	for {
-		ms, err := s.MessagesBefore(c.ID, lastMessageID, messagesFetchLimit)
-		if err != nil {
-			log.Fatal(err)
-		}
+	sort.Slice(ms, func(i, j int) bool {
+		return ms[i].Timestamp.Time().Before(ms[j].Timestamp.Time())
+	})
 
-		for _, m := range ms {
-			printMessage(&m)
-		}
-
-		if len(ms) > 0 {
-			break
-		}
-
-		lastMessageID = ms[len(ms)-1].ID
+	for _, m := range ms {
+		printMessage(&m)
 	}
 
 	s.Close()
 }
 
 func printMessage(m *discord.Message) {
-	name := m.Author.DisplayName
-	if name == "" {
-		name = m.Author.Username
+	fmt.Printf("[%s] %s %s (%s)",
+		m.ID, m.Timestamp.Format(time.DateTime), m.Author.Username, m.Author.ID)
+	if m.Pinned {
+		fmt.Println(" [pinned]")
 	}
 
-	fmt.Printf("[%s] %s %s (%s): ", m.ID, m.Timestamp.Format(time.DateTime), name, m.Author.ID)
 	switch m.Type {
 	case discord.CallMessage:
-		fmt.Printf("%s", "[started a call]")
-	case discord.DefaultMessage, discord.InlinedReplyMessage:
+		fmt.Printf("%s", " [started a call]")
+	case discord.RecipientAddMessage, discord.RecipientRemoveMessage:
+		if m.Type == discord.RecipientAddMessage {
+			fmt.Printf("%s", "[added")
+		} else {
+			fmt.Printf("%s", "[removed")
+		}
+		for _, u := range m.Mentions {
+			fmt.Printf(" %s (%s)", u.Username, u.ID)
+		}
+		fmt.Printf("%s", " <> group]")
+	case discord.ChannelNameChangeMessage:
+		fmt.Printf(" [changed group name to %s]", m.Content)
+	case discord.ChannelIconChangeMessage:
+		fmt.Printf(" [changed group icon]")
+	case discord.ChannelPinnedMessage:
+		fmt.Printf(" [pinned message %s]", m.ReferencedMessage.ID)
+	case discord.GuildMemberJoinMessage:
+		fmt.Printf(" [joined guild]")
+	case discord.DefaultMessage,
+		discord.InlinedReplyMessage,
+		discord.ChatInputCommandMessage:
 		if m.ReferencedMessage != nil {
-			fmt.Printf("[replying to message %s] ", m.ReferencedMessage.ID)
+			fmt.Printf(" [replying to message %s]", m.ReferencedMessage.ID)
 		}
 
-		fmt.Printf("%s", m.Content)
+		if m.Content != "" {
+			fmt.Printf(": %s", m.Content)
+		}
 
 		for _, a := range m.Attachments {
-			fmt.Printf(" [attachment: %s %s %s]", a.Filename, a.URL, a.Description)
+			fmt.Printf(" [attachment: %s (%s) %s %s]",
+				a.Filename, a.ContentType, a.URL, a.Description)
 		}
 		for _, s := range m.Stickers {
-			fmt.Printf(" [sticker: %s %s]", s.Name, s.ID)
+			fmt.Printf(" [sticker: %s (%s)]", s.Name, s.ID)
 		}
 		for _, r := range m.Reactions {
-			fmt.Printf(" [reaction: %d %s]", r.Count, r.Emoji.Name)
+			fmt.Printf(" [reaction: %d %s (%s)", r.Count, r.Emoji.Name, r.Emoji.ID)
+			if r.Me {
+				fmt.Printf(" reacted")
+			}
+			fmt.Printf("]")
 		}
 		for _, e := range m.Embeds {
-			fmt.Printf(" [embed: %s %s]", e.Title, e.Description)
+			fmt.Printf(" [embed:")
+			if e.Title != "" {
+				fmt.Printf(" %s", e.Title)
+			}
+			if e.Description != "" {
+				fmt.Printf(" %s", e.Description)
+			}
+			if e.Footer != nil {
+				fmt.Printf(" [footer: %s]", e.Footer.Text)
+			}
+			if e.Image != nil {
+				fmt.Printf(" [image: %s]", e.Image.URL)
+			}
+			if e.Thumbnail != nil {
+				fmt.Printf(" [thumbnail: %s]", e.Thumbnail.URL)
+			}
+			if e.Video != nil {
+				fmt.Printf(" [video: %s]", e.Video.URL)
+			}
+			for _, f := range e.Fields {
+				fmt.Printf(" [field: %s = %s]", f.Name, f.Value)
+			}
+			fmt.Printf("]")
 		}
 		if m.EditedTimestamp.IsValid() {
 			fmt.Printf(" [edited at %s]", m.Timestamp.Format(time.DateTime))
 		}
 	default:
-		fmt.Printf("[unhandled message type %d]", m.Type)
+		fmt.Printf("[unhandled message type %s]", typeNames[m.Type])
 	}
 	fmt.Println()
 }
